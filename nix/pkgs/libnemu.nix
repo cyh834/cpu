2b15b25f9b709df9bf3cc2c083318b26f3cebb19
cyh <1909625265@qq.com>
2024-11-16 18:22:43 +0800

    add NEWU tmp

--- nix/pkgs/libnemu.nix
+++ nix/pkgs/libnemu.nix
@@ -1,25 +1,32 @@
-{ stdenv, fetchFromGitHub}:
+{ stdenv, fetchFromGitHub, bison, which, pkg-config, readline, SDL2, zstd, gcc, gpp, gdb, flex, ccache}:
+
 stdenv.mkDerivation rec {
     # 指定包名和版本
     pname = "libnemu";
-    version = "master";
+    version = "ec9e1ca7627dd35a7012ed019bd40e5ee50e567b";
 
     # 从 GitHub 下载源代码
-    src = fetchFromGitHub {
-        owner = "OpenXiangShan";
-        repo = "NEMU";
-        rev = version;
-        fetchSubmodules = false;
-        sha256 = "sha256-c/UG6aYYhKYdiJEzSu4V9TjLwX8eN2hp9lD53TFaEt0=";
-    };
+    #src = fetchFromGitHub {
+    #    owner = "cyh834";
+    #    repo = "NEMU";
+    #    rev = version;
+    #    sha256 = "sha256-zbyQkGW+WzyVLEpJRnKxD5pqz/nFsVMMS8bMnTNYD6U=";
+    # };
+    src = ../../NEMU;
 
-    patches = [
-    ../patches/nemu/add_config.patch
-    ];
+    # 应用补丁
+    # Patches = [
+    # ../patches/nemu/add_config.patch
+    # ];
+
+    # 添加构建依赖
+    nativeBuildInputs = [ bison which pkg-config flex];
+    buildInputs = [ readline SDL2 zstd gcc gpp gdb ccache];
 
     buildPhase = ''
         runHook preBuild
-        export NEMU_HOME=$TMPDIR
+        export NEMU_HOME=$PWD
+        
         make riscv64-cyh-ref_defconfig
         make -j
         runHook postBuild
@@ -28,7 +35,7 @@ stdenv.mkDerivation rec {
     installPhase = ''
         runHook preInstall
         mkdir -p $out/lib
-        mv $TMPDIR/build/nemu-interpreter.so $out/lib/nemu-interpreter.so
+        mv build/nemu-interpreter.so $out/lib/nemu-interpreter.so
         runHook postInstall
     '';
 }
