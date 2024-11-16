22453fe3a8521be4ff9f7caf6a245209a966d7b8
cyh <1909625265@qq.com>
2024-11-16 16:56:18 +0800

    add patch

--- nix/pkgs/libnemu.nix
+++ nix/pkgs/libnemu.nix
@@ -1,12 +1,34 @@
-{ stdenv, fetchFromGitHub, make }:
+{ stdenv, fetchFromGitHub}:
 stdenv.mkDerivation rec {
+    # 指定包名和版本
     pname = "libnemu";
     version = "master";
-    nativeBuildInputs = [ make ];
+
+    # 从 GitHub 下载源代码
     src = fetchFromGitHub {
-        owner = "cyh834";
+        owner = "OpenXiangShan";
         repo = "NEMU";
         rev = version;
-        sha256 = "";
+        fetchSubmodules = false;
+        sha256 = "sha256-c/UG6aYYhKYdiJEzSu4V9TjLwX8eN2hp9lD53TFaEt0=";
     };
+
+    patches = [
+    ../patches/nemu/add_config.patch
+    ];
+
+    buildPhase = ''
+        runHook preBuild
+        export NEMU_HOME=$TMPDIR
+        make riscv64-cyh-ref_defconfig
+        make -j
+        runHook postBuild
+    ''; 
+
+    installPhase = ''
+        runHook preInstall
+        mkdir -p $out/lib
+        mv $TMPDIR/build/nemu-interpreter.so $out/lib/nemu-interpreter.so
+        runHook postInstall
+    '';
 }
